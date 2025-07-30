0ef1c4d14234d90cc768566c11fd38be53dafe07
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-07-30 15:38:38 -0700

    Basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [cost, setCost] = useState();
+  const [interest, setInterest] = useState(10);
+  const [fee, setFee] = useState(0);
+  const [downPayment, setDownPayment] = useState(0);
+  const [emi, setEmi] = useState(0);
+  const [tenure , setTenure] = useState(0);
 
   return (
-    <h1>EMI Calculator</h1>
+    <div className='App'>
+      <span className='title' style={{ fontSize:30 , marginTop: 10}}>EMI Calculator</span>
+
+      <span className='title'>Total Cost of Asset</span>
+      <input 
+        type="number"
+        value={cost}
+        onChange={(e) => setCost(e.target.value)}
+        placeholder="Total Cost of Asset" 
+      />
+
+      <span className='title'>Interest (in %)</span>
+      <input 
+        type="number"
+        value={interest}
+        onChange={e => setInterest(e.target.value)}
+        placeholder='Interest (in %)' 
+      />
+
+      <span className='title'>Processing Fee (in %)</span>
+      <input 
+        type="number"
+        value={fee}
+        onChange={e => setFee(e.target.value)}
+        placeholder='Processing Fee (in %)' 
+      />
+
+      <span className='title'>Down Payment</span>
+      {/* slider */}
+
+      <span className='title'>Loan per Month</span>
+      {/* slider */}
+
+      <span className='title'>Tenure</span>
+
+    </div>
   )
 }
 
