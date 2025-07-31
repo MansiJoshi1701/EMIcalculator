72ccf4ec7234615bd0aef7fa3add56523fa18ed5
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-07-31 16:14:28 -0700

    Slider inputs added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,32 @@ function App() {
   const [emi, setEmi] = useState(0);
   const [tenure , setTenure] = useState(0);
 
+
+  const calculateEMI = () => {}
+
+  const updateDownPayment = (e) => {
+
+    if(!cost) return
+
+    const EMI = Number(e.target.value);
+    setEmi(EMI);
+
+    //Calculate down payment and update it
+    
+  }
+
+  const updateEMI = (e) => {
+
+    if(!cost) return
+
+    const DP = Number(e.target.value);
+    setDownPayment(DP);
+
+    //Calculate EMI and update it
+
+  }
+
+
   return (
     <div className='App'>
       <span className='title' style={{ fontSize:30 , marginTop: 10}}>EMI Calculator</span>
@@ -18,7 +44,7 @@ function App() {
       <input 
         type="number"
         value={cost}
-        onChange={(e) => setCost(e.target.value)}
+        onChange={e => setCost(e.target.value)}
         placeholder="Total Cost of Asset" 
       />
 
@@ -39,11 +65,41 @@ function App() {
       />
 
       <span className='title'>Down Payment</span>
-      {/* slider */}
+      <div>{downPayment}</div>
+      <div>
+        <input 
+          type="range"
+          min="0"
+          max={cost}
+          value={downPayment}
+          onChange={updateEMI}
+          className='slider'
+        />
+        <div className='lables'>
+          <label>0%</label>
+          <label>{downPayment}</label>
+          <label>100%</label>
+        </div>
+      </div>
+
 
       <span className='title'>Loan per Month</span>
-      {/* slider */}
-
+      <div>{emi}</div>
+      <div>
+        <input 
+          type="range"
+          min={calculateEMI(cost)}
+          max={calculateEMI(0)}
+          value={emi}
+          onChange={updateDownPayment}
+          className='slider'
+        />
+        <div className='lables'>
+          <label>{calculateEMI(cost)}</label>
+          <label>{emi}</label>
+          <label>{calculateEMI(0)}</label>
+        </div>
+      </div>
       <span className='title'>Tenure</span>
 
     </div>
